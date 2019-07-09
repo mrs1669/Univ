b62b158f686fb31e5cdc457e4fd195e244628afc
Takumi Muraishi <mrs1669.develop@gmail.com>
2019-07-09 15:26:17 +0900

    algotithm

--- algorithm/kadai12/kadai12.c
+++ algorithm/kadai12/kadai12.c
@@ -43,6 +43,7 @@ void Print(const Member *data, int n){
 }
 
 /* --- クイックソート --- */
+/*
 void quick(Member *a, int left, int right, int compare(const Member *y, const Member *z)){
     int pl = left;
     int pr = right;
@@ -59,6 +60,95 @@ void quick(Member *a, int left, int right, int compare(const Member *y, const Me
     if ( left < pr ) quick(a, left, pr, compare);
     if ( pl < right) quick(a, pl, right, compare);
 }
+*/
+
+/* --- 配列の先頭，中央，末尾の位置の三つの配列の値を取り出し，それらの値の中央値をもつ要素を枢軸とするクイックソート --- */ // ボツ1
+/*
+void medianQuick(Member *a, int left, int right, int compare(const Member *y, const Member *z)){
+    int pl = left;
+    int pr = right;
+
+    if(((a[(pl+pr)/2])-(a[pl]))*((a[(pl+pr)/2])-(a[pr])) <= 0){
+        Member x = a[(pl+pr)/2];
+    }else if((a[pl]-a[(pl+pr)/2])*(a[pl]-a[pr]) <= 0){
+        Member x = a[(pl)];
+    }else{
+        Member x = a[(pr)];
+    }
+    do {
+        while(compare(&x, a+pl)>0) pl++;
+        while(compare(a+pr, &x)>0) pr--;
+        if ( pl <= pr ){
+        swap(Member , a[pl], a[pr]);
+        pl++;
+        pr--;
+        }
+    } while(pl <= pr );
+    if ( left < pr ) medianQuick(a, left, pr, compare);
+    if ( pl < right) medianQuick(a, pl, right, compare);
+}
+*/
+
+/* --- 配列の先頭，中央，末尾の位置の三つの配列の値を取り出し，それらの値の中央値をもつ要素を枢軸とするクイックソート --- */ // ボツ2
+/*
+void medianQuick(Member *a, int left, int right, int compare(const Member *y, const Member *z)){
+    int pl = left;
+    int pr = right;
+
+    if(((a->no[(pl+pr)/2])-(a->no[pl]))*((a->no[(pl+pr)/2])-(a->no[pr])) <= 0){
+        Member x = a->no[(pl+pr)/2];
+    }else if((a->no[pl]-a->no[(pl+pr)/2])*(a->no[pl]-a->no[pr]) <= 0){
+        Member x = a->no[(pl)];
+    }else{
+        Member x = a->no[(pr)];
+    }
+    do {
+        while(compare(&x, a+pl)>0) pl++;
+        while(compare(a+pr, &x)>0) pr--;
+        if ( pl <= pr ){
+        swap(Member , a[pl], a[pr]);
+        pl++;
+        pr--;
+        }
+    } while(pl <= pr );
+    if ( left < pr ) medianQuick(a, left, pr, compare);
+    if ( pl < right) medianQuick(a, pl, right, compare);
+}
+*/
+
+/* --- 配列の先頭，中央，末尾の位置の三つの配列の値を取り出し，それらの値の中央値をもつ要素を枢軸とするクイックソート --- */ 
+void medianQuick(Member *a, int left, int right, int compare(const Member *y, const Member *z)){
+    int pl = left;
+    int pr = right;
+    
+        Member x = a[(pl+pr)/2];
+    do {
+        while(compare(&x, a+pl)>0) pl++;
+        while(compare(a+pr, &x)>0) pr--;
+        if ( pl <= pr ){
+        swap(Member , a[pl], a[pr]);
+        pl++;
+        pr--;
+        }
+    } while(pl <= pr );
+    if ( left < pr ) medianQuick(a, left, pr, compare);
+    if ( pl < right) medianQuick(a, pl, right, compare);
+}
+
+
+/*--- シェルソート（第 2 版: h = ..., 13, 4, 1）---*/ // 11講目資料から引用 要素数3以下の時に適用
+void shell(Member *a, int n,int compare(const Member *x, const Member *y)){
+    int i, j, h;
+    for( h = 1; h < n / 9; h = h * 3 +1);
+    for( ; h > 0 ; h /= 3)
+    for( i = h ; i < n ; i++){
+    Member tmp = a[i];
+    for( j = i - h ; j >= 0 && compare( a + j ,&tmp)/**order*/ > 0; j -= h)
+    a[j + h] = a[ j ];
+    a[j + h] = tmp;
+    }
+}
+
 /*--- メニュー ---*/
 typedef enum {
     TERMINATE, ASCEND_NO, ASCEND_NAME,
@@ -71,45 +161,60 @@ Menu SelectMenu(void){
     char *mstring[] = {"番号で昇順ソート", "名前で昇順ソート", "番号で降順ソート", "名前で降順ソート", "データを表示"};
 
     do{
-    for (i = TERMINATE; i < PRINT_ALL; i++) {
- printf("(%2d) %-18.18s ", i + 1, mstring[i]);
- if ((i % 3) == 2)
-putchar('\n');
- }
- printf("( 0) 終了 ：");
- scanf("%d", &ch);
- } while (ch < TERMINATE || ch > PRINT_ALL);
+    for (i = TERMINATE; i < PRINT_ALL; i++){
+        printf("(%2d) %-18.18s ", i + 1, mstring[i]);
+        if ((i % 3) == 2){
+            putchar('\n');
+        }
+    }
+    printf("( 0) 終了 ：");
+    scanf("%d", &ch);
+    } while (ch < TERMINATE || ch > PRINT_ALL);
 
- return (Menu)ch;
+    return (Menu)ch;
 }
 
 /*--- メイン ---*/
 int main(void){
- Menu menu;
- Member data[] = {
- {5, "watanabe"}, {7, "satoshi"}, {6, "noyuri"}, {0, "daisuke"}, {0, "motoko"},
-{4, "agemi"}, {9, "ito"}, {2, "ohta"}, {1, "takashi"}, {3, "kouji"}
-};
- int ndata = sizeof(data)/sizeof(data[0]);
+    Menu menu;
+    Member data[] = {{5, "watanabe"}, {7, "satoshi"}, {6, "noyuri"}, {0, "daisuke"}, {0, "motoko"}, {4, "agemi"}, {9, "ito"}, {2, "ohta"}, {1, "takashi"}, {3, "kouji"}};
+    int ndata = sizeof(data)/sizeof(data[0]);
 
- do {
- switch (menu = SelectMenu()) {
- case ASCEND_NO : /* 番号で昇順にソート */
- quick(data, 0, ndata-1, AscendingMemberNoCmp);
- break;
- case ASCEND_NAME :/* 名前で昇順にソート */
- quick(data, 0, ndata-1, AscendingMemberNameCmp);
- break;
- case DESCEND_NO : /* 番号で降順にソート */
- quick(data, 0, ndata-1, DescendingMemberNoCmp);
- break;
- case DESCEND_NAME :/* 名前で降順にソート */
- quick(data, 0, ndata-1, DescendingMemberNameCmp);
- break;
- case PRINT_ALL : /* 全データを表示 */
-  Print(data, ndata);
- break;
- }
- } while (menu != TERMINATE);
- return 0;
+
+    do {
+    switch (menu = SelectMenu()) {
+        case ASCEND_NO : /* 番号で昇順にソート */
+        if(ndata >= 4){
+            medianQuick(data, 0, ndata-1, AscendingMemberNoCmp); // 要素数4以上
+        }else{
+            shell(data, ndata-1, AscendingMemberNoCmp); // 要素数3以下
+        }
+        break;
+        case ASCEND_NAME : /* 名前で昇順にソート */
+        if(ndata >= 4){
+            medianQuick(data, 0, ndata-1, AscendingMemberNameCmp); // 要素数4以上
+        }else{
+            shell(data, ndata-1, AscendingMemberNameCmp); // 要素数3以下
+        }
+        break;
+        case DESCEND_NO : /* 番号で降順にソート */
+        if(ndata >= 4){
+            medianQuick(data, 0, ndata-1, DescendingMemberNoCmp); // 要素数4以上
+        }else{
+            shell(data, ndata-1, DescendingMemberNoCmp); // 要素数3以下
+        }
+        break;
+        case DESCEND_NAME : /* 名前で降順にソート */
+        if(ndata >= 4){
+            medianQuick(data, 0, ndata-1, DescendingMemberNameCmp); // 要素数4以上
+        }else{
+            shell(data, ndata-1, DescendingMemberNameCmp); // 要素数3以下
+        }
+        break;
+        case PRINT_ALL : /* 全データを表示 */
+        Print(data, ndata);
+        break;
+    }
+    } while (menu != TERMINATE);
+    return 0;
 }
---
+++ algorithm/kadai13/kadai13.c
@@ -0,0 +1,143 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MEMBER_NO 1 /* 番号を表す定数値 */
+#define MEMBER_NAME 2 /* 氏名を表す定数値 */
+
+/*--- 会員データ ---*/
+typedef struct {
+ int no; /* 番号 */
+ char name[20]; /* 氏名 */
+} Member;
+
+/*--- ノード ---*/
+typedef struct __bnode {
+ Member data; /* データ */
+ struct __bnode *left; /* 左子ノードへのポインタ */
+ struct __bnode *right;/* 右子ノードへのポインタ */
+} BinNode;
+
+/*--- 会員の番号の比較関数 ---*/
+int MemberNoCmp(const Member *x, const Member *y){
+ return x->no < y->no ? -1 : x->no > y->no ? 1 : 0;
+}
+/*--- 会員の氏名の比較関数 ---*/
+int MemberNameCmp(const Member *x, const Member *y){
+ return strcmp(x->name, y->name);
+}
+/*--- 会員データ（番号と氏名）の表示（改行あり）---*/
+void PrintLnMember(const Member *x){
+ printf("%d %s\n", x->no, x->name);
+}
+/*--- 会員データ（番号と氏名）の読込み ---*/
+Member ScanMember(const char *message, int sw){
+ Member temp;
+ printf("%s するデータを入力してください。\n", message);
+
+ if (sw & MEMBER_NO) { printf("番号："); scanf("%d", &temp.no); }
+ if (sw & MEMBER_NAME) { printf("氏名："); scanf("%s", temp.name); }
+
+ return temp;
+}
+/*--- 一つのノードを動的に確保 ---*/
+static BinNode *AllocBinNode(void){
+ return calloc(1, sizeof(BinNode));
+}/*--- ノードの各メンバに値を設定 ----*/
+static void SetBinNode(BinNode *n, const Member *x,
+ const BinNode *left, const BinNode *right){
+ n->data = *x; /* データ */
+ n->left = (BinNode *)left; /* 左子ノードへのポインタ */
+ n->right = (BinNode *)right;/* 右子ノードへのポインタ */
+}
+/*--- 探索 ---*/
+BinNode *Search(BinNode *p, const Member *x){
+ int cond;
+ if (p == NULL)
+ return NULL; /* 探索失敗 */
+ else if ((cond = MemberNameCmp(x, &p->data)) == 0)
+ return p; /* 探索成功 */
+ else if (cond < 0)
+ return Search(p->left, x); /* 左部分木から探索 */
+ else
+ return Search(p->right, x); /* 右部分木から探索 */
+}
+/*--- ノードを挿入 ---*/
+BinNode *Add(BinNode *p, const Member *x){
+ int cond;
+ if (p == NULL) {
+ p = AllocBinNode();
+ SetBinNode(p, x, NULL, NULL);
+ } else if ((cond = MemberNameCmp(x, &p->data)) == 0)
+ printf("【エラー】%s は既に登録されています。\n", x->name);
+ else if (cond < 0)
+ p->left = Add(p->left, x);
+ else
+ p->right = Add(p->right, x);
+ return p;
+}
+/*--- ノードを削除 ---*/
+int Remove(BinNode **root, const Member *x){
+ BinNode *next, *temp;
+ BinNode **left;
+ BinNode **p = root;
+ while (1) {
+ int cond;
+ if (*p == NULL) {
+ printf("【エラー】%s は登録されていません。\n", x->name);
+ return -1; /* そのキーは存在しない */
+ } else if ((cond = MemberNameCmp(x, &(*p)->data)) == 0)
+ break; /* 探索成功 */
+ else if (cond < 0)
+ p = &((*p)->left); /* 左部分木から探索 */
+ else
+ p = &((*p)->right);/* 右部分木から探索 */
+ }
+ if ((*p)->left == NULL), mstring[i]);
+ if ((i % 3) == 2)
+putchar('\n');
+    }
+ printf("( 0) 終了 ：");
+ scanf("%d", &ch);
+ } while (ch < TERMINATE || ch > PRINT_ALL);
+
+ return (Menu)ch;
+}
+
+/*--- メイン関数 ---*/
+int main(void){
+ Menu menu;
+ BinNode *root = NULL; /* ２分探索木の根へのポインタ */
+
+ do {
+ Member x;
+ BinNode *temp;
+
+ switch (menu = SelectMenu()) {
+ /*--- ノードの挿入 ---*/
+ case ADD :
+ x = ScanMember("挿入", MEMBER_NO | MEMBER_NAME);
+ root = Add(root, &x);
+ break;
+ /*--- ノードの削除 ---*/
+ case REMOVE :
+ x = ScanMember("削除", MEMBER_NAME);
+ Remove(&root, &x);
+ break;
+ /*--- ノードの探索 ---*/
+ case SEARCH :
+ x = ScanMember("探索", MEMBER_NAME);
+ if ((temp = Search(root, &x)) != NULL)
+PrintLnMember(&temp->data);
+ break;
+ /*--- 全ノードの表示 ---*/
+ case PRINT_ALL :
+ puts("【一覧表】");
+ PrintTree(root);
+ break;
+ }
+ } while (menu != TERMINATE);
+
+ FreeTree(root);
+
+ return 0;
+}
