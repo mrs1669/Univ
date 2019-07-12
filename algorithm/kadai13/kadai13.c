aea9019eaf69ce474c9047a739fa7e36adfa532f
Takumi Muraishi <mrs1669.develop@gmail.com>
2019-07-12 15:32:54 +0900

    探索まで

--- algorithm/kadai13/kadai13.c
+++ algorithm/kadai13/kadai13.c
@@ -27,3 +27,45 @@ int MemberNoCmp(const Member *x, const Member *y){
 int MemberNameCmp(const Member *x, const Member *y){
     return strcmp(x->name, y->name);
 }
+
+/*--- 会員データ（番号と氏名）の表示（改行あり）---*/
+void PrintLnMember(const Member *x){
+    printf("%d %s\n", x->no, x->name);
+}
+
+/*--- 会員データ（番号と氏名）の読込み ---*/
+Member ScanMember(const char *message, int sw){
+    Member temp;
+    printf("%s するデータを入力してください。\n", message);
+
+    if (sw & MEMBER_NO) { printf("番号："); scanf("%d", &temp.no); }
+    if (sw & MEMBER_NAME) { printf("氏名："); scanf("%s", temp.name); }
+
+    return temp;
+}
+
+/*--- 一つのノードを動的に確保 ---*/
+static BinNode *AllocBinNode(void){
+    return calloc(1, sizeof(BinNode));
+}
+
+/*--- ノードの各メンバに値を設定 ----*/
+static void SetBinNode(BinNode *n, const Member *x,
+    const BinNode *left, const BinNode *right){
+    n->data = *x; /* データ */
+    n->left = (BinNode *)left; /* 左子ノードへのポインタ */
+    n->right = (BinNode *)right;/* 右子ノードへのポインタ */
+}
+
+/*--- 探索 ---*/
+BinNode *Search(BinNode *p, const Member *x){
+    int cond;
+    if (p == NULL)
+    return NULL; /* 探索失敗 */
+    else if ((cond = MemberNameCmp(x, &p->data)) == 0)
+    return p; /* 探索成功 */
+    else if (cond < 0)
+    return Search(p->left, x); /* 左部分木から探索 */
+    else
+ return Search(p->right, x); /* 右部分木から探索 */
+}
